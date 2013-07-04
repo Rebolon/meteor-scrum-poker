aa4327de57c5b5ad91c95378469d436b09d555cc
rebolon <richard.tribes@gmail.com>
2013-07-04 11:27:40 +0000

    debut de mise en place du nikoniko

---
+++ client/views/meeting/member_list.js
@@ -0,0 +1,29 @@
+Template.memberList.helpers({
+	member: function funcGetMemberListOfMeeting() {
+                var members = [],
+		    meeting = Meeting.findOne({_id: Session.get('selectedMeeting')});
+		// manage first time tempalte is rendered whereas data is maybe not arrived
+		if (meeting) {
+			members = meeting.members;
+		}
+
+                return members;
+        },
+
+	selectedMeeting: function () {
+		var selectedMeeting = Session.get('selectedMeeting');
+                if (selectedMeeting) {
+			return true;
+                }
+		return false;
+	}
+});
+
+Template.memberList.events({
+	'click #btnGotoSummary': function funcGotoSummary() {
+		var selectedMeeting = Session.get('selectedMeeting');
+		if (selectedMeeting) {
+			Meteor.Router.to('/summary/' + selectedMeeting);
+		}
+	}
+});
